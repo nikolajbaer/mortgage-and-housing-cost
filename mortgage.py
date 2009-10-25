6224c3ee685e31319601959163dc15443ca30599
Nikolaj Baer <nikolaj@stinkpad.(none)>
2009-10-24 18:31:57 -0700

    changed so payments and balances are calculated over time. Added equity growth, and starting to add in income growth, as wellas account for necessary things deducted from income (e.g. social security and insurance)

--- mortgage.py
+++ mortgage.py
@@ -1,9 +1,16 @@
 """Mortgage and Home Ownership calculations
 
-This program is my attempt to understand and calculate the cost of owning a home.
+This program is my attempt to understand and calculate the cost of owning a home. I skipped on the spreadsheets because i want to build it myself in the way that i usually comprehend things.. through code.
 
 Author: Nikolaj Baer
 License: MIT License
+
+References;
+http://www.hughchou.org/calc/formula.html
+http://en.wikipedia.org/wiki/Amortization_calculator
+http://en.wikipedia.org/wiki/Amortization_(business)
+http://en.wikipedia.org/wiki/Tax_bracket#Tax_brackets_in_the_United_States
+http://www.ftb.ca.gov/forms/2008_California_Tax_Rates_and_Exemptions.shtml
 """
 
 FED_TAX_TABLE=(
@@ -49,6 +56,13 @@ CA_TAX_TABLE=(
         (2339.99,9.30,64050.00,1000000000.00), ),
 )
 
+FICA_RATE="7.65"
+FICA_MAX="6621.60"
+
+# average yearly inflation from BLS in USA from 1998-2009
+USA_INFLATION_RATES=[3.85,2.85,3.24,3.39,2.68,2.27,1.59,2.83,3.38,2.19,1.55]
+USA_INFLATION_RATE=sum(USA_INFLATION_RATES)/len(USA_INFLATION_RATES)
+
 def calculate_monthly_cost(value,monthly_payment,prop_taxes,repair_cost,insurance,utilities):
     costs=monthly_payment
     costs+=value*prop_taxes/100/12.0
@@ -57,24 +71,41 @@ def calculate_monthly_cost(value,monthly_payment,prop_taxes,repair_cost,insuranc
     costs+=utilities
     return costs
 
-def calculate_payment(principal,interest,years):
-    monthly_interest=interest/100.0/12
-    return principal * monthly_interest/(1-(1+monthly_interest) ** (-years*12))
+def amortize(balance,monthly_interest,monthly_payment):
+    interest_paid=balance*monthly_interest  
+    equity_earned=monthly_payment-interest_paid
+    new_balance=balance-equity_earned
+    return new_balance,interest_paid,equity_earned
 
+def generate_pay_schedule(principal,interest,years):
+    monthly_interest=interest/100.0/12
+    monthly_payment=principal * monthly_interest/(1-(1+monthly_interest) ** (-years*12))
+    balances,interest_paid,equity_earned=[],[],[]
+    # smart people do this in formulas, but i kindof like 
+    # the iterative one as it gives me more of a sense of how it compounds 
+    b,i,q=principal,0.0,0.0
+    for y in range(years*12):
+        b,i,q=amortize(b,monthly_interest,monthly_payment) 
+        balances.append(b)
+        interest_paid.append(i)
+        equity_earned.append(q)
+    return balances,interest_paid,equity_earned,monthly_payment
+
+def get_tax(i,fs):
+    tr=0.0
+    for r in FED_TAX_TABLE:
+        if r[fs][0]<=i and r[fs][1] >= i:
+            tr=r[0]
+            break
+    extra=0.0
+    for r in CA_TAX_TABLE[fs-1]:
+         if r[2]<=i and r[3] >= i:
+            tr+=r[1]
+            extra+=r[0]
+    print "got tax rate of ",tr,"% plus extra ",extra
+    return ((tr/100.0)*i) + extra
+    
 def calculate_tax_savings(agi,writeoffs,filing_status):
-    def get_tax(i,fs):
-        tr=0.0
-        for r in FED_TAX_TABLE:
-            if r[fs][0]<=i and r[fs][1] >= i:
-                tr=r[0]
-                break
-        extra=0.0
-        for r in CA_TAX_TABLE[filing_status-1]:
-             if r[2]<=i and r[3] >= i:
-                tr+=r[1]
-                extra+=r[0]
-        print "got tax rate of ",tr,"% plus extra ",extra
-        return ((tr/100.0)*i) + extra
     taxes=get_tax(agi,filing_status)
     print "initial taxes ",taxes
     n_agi=(agi-writeoffs)
@@ -90,21 +121,50 @@ def build_optparser():
     parser.add_option("-r","--rate",dest="rate",help="Interest rate of mortgage as percent (e.g. 5 for 5%)",default="5")
     parser.add_option("-y","--years",dest="years",help="Years of mortgage",default="30")
     parser.add_option("-p","--property-tax-rate",dest="prop_taxes",help="Property tax as percent of home value",default="1.5")
-    parser.add_option("--repair-cost",dest="repair",help="Annual maintenace costs as percentage of home value per year",default="2")
+    parser.add_option("--repair-cost",dest="repair",help="Annual maintenace costs as percentage of home value (including equity growth) per year",default="2")
     parser.add_option("--insurance",dest="insurance",help="Annual cost of homeowners insurance in dollars",default="2000")
     parser.add_option("-u","--utilites",dest="utilities",help="Monthly average cost of utilities",default="150")
     parser.add_option("--filing-status",dest="filing_status",help="Tax filing status. 1=single, 2=joint,3=separate,4=head of household.",default="2")
+    parser.add_option("--equity-growth",dest="equity_growth",help="Annual percentage increase in value of house",default="1")
+    parser.add_option("--inflation-rate",dest="inflation_rate",help="Rate of inflation as it effects utilities and insurance",default="%0.2f"%USA_INFLATION_RATE)
+    parser.add_option("--salary-growth",dest="salary_growth",help="Rate of salary growth in percent",default="3")
+    parser.add_option("--medical-insurance",dest="med_insurance",help="monthly cost of medical insurance",default="150")
+
     return parser
 
-def do_the_math(value,agi,options):
-    # Start with monthly payment  
+def do_the_math(value,income,options):
+    # Start with monthly payment, and payments over time
     down,rate,years=float(options.down),float(options.rate),float(options.years)
     principal=value*(1-down/100.0)
     vals={"principal":principal,"down payment":down/100.0*value,"rate":rate,"years":years}
-    mp=calculate_payment(principal,rate,years)
+    data={}
+    # Here I calculate the equity growth over time, as well as the monthly payment
+    balances,interest_paid,equity_earned,mp=generate_pay_schedule(principal,rate,years)
+    # store these lists of numbers so i can graph stuff
+    data["balances"]=balances
+    data["interest paid"]=interest_paid
+    data["equity earned"]=equity_earned
+    data["monthly payment"]=[mp]*int(years)*12
+
+    interest_writeoff=[]
+    monthly_income=[]
+    stated_income=[]
+    income_over_time=income
+    for y in range(years):
+        income_over_time*=1.0+(float(options.salary_growth)/100.0)
+        writeoff=sum(interest_paid[y*12:(y+1)*12])
+        agi=income_over_time-writeoff
+        income_tax=get_tax(agi,int(options.filing_status))
+        monthly_income += [(income_over_time-income_tax)/12.0]*12
+        stated_income +=[income_over_time/12.0]*12
+        interest_writeoff=[writeoff/12.0]*12
+
+    data["monthly_income"]=monthly_income
+    data["interest_writeoff"]=interest_writeoff
+    data["stated income"]=stated_income
+    vals["monthly payment"]=mp
 
     # Then monthly costs
-    vals["monthly payment"]=mp
     vals["total cost"]=mp*years*12
     vals["total interest"]=(mp*years*12)-principal
     mc=calculate_monthly_cost(  value,
@@ -113,19 +173,28 @@ def do_the_math(value,agi,options):
                                 float(options.repair),
                                 float(options.insurance),
                                 float(options.utilities) )
+
     vals["monthly costs"]=mc
 
     # Figure out extra income from writeoffs
-    vals["extra monthly income"]=calculate_tax_savings(agi,vals["total interest"]/years,int(options.filing_status))
+    vals["extra monthly income"]=calculate_tax_savings(income,vals["total interest"]/years,int(options.filing_status))
 
     vals["monthly costs with new income"]=mc-vals["extra monthly income"] 
     # return the values we calculated
-    return vals
+    return vals,data
 
 if __name__=="__main__":
     parser=build_optparser()
     (options,args)=parser.parse_args()
-    vals=do_the_math(float(args[0]),float(args[1]),options) 
+    vals,data=do_the_math(float(args[0]),float(args[1]),options) 
     print "\n","*"*50
     for k,v in vals.items():
-        print "%s: %0.2f"%(k,v)
+        if type(v)==float:
+            print "%s: %0.2f"%(k,v)
+            
+    print "*"*100,"\nPayment Schedule\n","*"*100 
+    for k,v in data.items():
+        print k
+        print ",".join(["%.2f"%i for i in v[:5]]),"...",",".join(["%.2f"%i for i in v[-5:]])
+ 
+
